44899b48dc605efb987157b2bf6450ad3eefcfdf
Allen Chang <achang24678@gmail.com>
2019-04-15 21:58:17 -0700

    firebase issues

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,7 +7,7 @@ import 'firebase/auth';
 const config = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL:`https://blog-app-9a306.firebaseio.com`,
+    databaseURL:process.env.FIREBASE_DATABASE_URL,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_SOTRAGE_BUCKE,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
