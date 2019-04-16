dd8a95332adc4af8a0838de23063f2ec3e50c5c0
Allen Chang <achang24678@gmail.com>
2019-04-15 22:26:34 -0700

    firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,8 +5,8 @@ import 'firebase/auth';
 
 // Initialize Firebase
 const config = {
-    apiKey: "AIzaSyDMEHPiQxvP1svyyXlAqD3KBu-t0_Ai2cU",
-    authDomain: "blog-app-9a306.firebaseapp.com",
+    apiKey:"AIzaSyDMEHPiQxvP1svyyXlAqD3KBu-t0_Ai2cU",
+    authDomain:"blog-app-9a306.firebaseapp.com",
     databaseURL: "https://blog-app-9a306.firebaseio.com",
     projectId: "blog-app-9a306",
     storageBucket: "blog-app-9a306.appspot.com",
