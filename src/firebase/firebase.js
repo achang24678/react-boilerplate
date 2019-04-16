515661a191c5c5d0fa2698a753290c9fed0701f4
Allen Chang <achang24678@gmail.com>
2019-04-15 22:17:40 -0700

    firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,11 +5,11 @@ import 'firebase/auth';
 
 // Initialize Firebase
 const config = {
-    apiKey:process.env.FIREBASE_API_KEY,
+    apiKey: process.env.FIREBASE_API_KEY,
     authDomain:process.env.FIREBASE_PROJECT_ID,
     databaseURL:`https://blog-app-9a306.firebaseio.com`,
     projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_SOTRAGE_BUCKE,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
 };
 firebase.initializeApp(config);
