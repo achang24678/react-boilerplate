b8cc968b1c4b4ca735b7b474ea16c09a61db52be
Allen Chang <achang24678@gmail.com>
2019-04-15 22:01:25 -0700

    firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,7 +5,7 @@ import 'firebase/auth';
 
 // Initialize Firebase
 const config = {
-    apiKey: process.env.FIREBASE_API_KEY,
+    apiKey:process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     databaseURL:process.env.FIREBASE_DATABASE_URL,
     projectId: process.env.FIREBASE_PROJECT_ID,
